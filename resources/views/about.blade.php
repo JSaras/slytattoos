28062873778395f090d475b74156afe916539a23
Julia <julialanu@hotmail.com>
2019-11-01 00:17:16 +0100

    test commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <h1>Welcome to the about page</h1>
-    <p>About SlyTattoos: <br> Sly tattoos is een tattooshop die zich bevind in Rijswijk
+    <p>About SlyTattoos: <br><br> Sly tattoos is een tattooshop die zich bevind in Rijswijk
     </p>
 @endsection
