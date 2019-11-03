626d2418fb0f92942300b73abe2296ef85df0a5b
Julia <julialanu@hotmail.com>
2019-11-03 21:08:22 +0100

    mistake fixed

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -20,13 +20,18 @@ class AdminLoginController extends Controller
     public function login(Request $request){
         $this->validate($request, [
             'email' => 'required|email',
-            'password' => 'required|min:6'
+            'password' => 'required'
         ]);
 
-        if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)); {
-            return redirect()->intended(route('admin.dashboard'));
-        }
+        // if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)); {
+        //     return redirect()->intended(route('admin.dashboard'));
+        // }
         
+        if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+            // if successful then redirect to their intended location
+         redirect()->intended(route('admin.dashboard'));
+        }
+
         return redirect()->back()->withInput($request->only('email', 'remember'));
     }
 }
